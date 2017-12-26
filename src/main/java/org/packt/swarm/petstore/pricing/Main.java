20bcfb97c48197472d7a5c22d39d069b9c5a08a6
Tomasz Adamski <tadamski@redhat.com>
2017-12-26 19:40:18 +0100

    Remove debug

--- src/main/java/org/packt/swarm/petstore/pricing/Main.java
+++ src/main/java/org/packt/swarm/petstore/pricing/Main.java
@@ -6,10 +6,7 @@ import org.wildfly.swarm.datasources.DatasourcesFraction;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
-        System.out.println("HOST TO "+System.getenv("POSTGRESQL_HOST"));
-        System.out.println("WEBHOOK TEST");
-
+        
         DatasourcesFraction datasourcesFraction = new DatasourcesFraction()
                 //1
                 .jdbcDriver("postgres", (d) -> {
